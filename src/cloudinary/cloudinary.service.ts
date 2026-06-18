36414dd0d7265d7145476b792897fd1f505320ac
imamhossenbu <imam.cse9.bu@gmail.com>
2026-06-17 22:15:37 -0600

    add cloudinary service

---
+++ src/cloudinary/cloudinary.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { v2 as cloudinary } from 'cloudinary';
+import { UploadApiErrorResponse, UploadApiResponse } from 'cloudinary';
+import * as streamifier from 'streamifier';
+
+@Injectable()
+export class CloudinaryService {
+  uploadFile(
+    file: Express.Multer.File,
+  ): Promise<UploadApiResponse | UploadApiErrorResponse> {
+    return new Promise((resolve, reject) => {
+      const upload = cloudinary.uploader.upload_stream((error, result) => {
+        if (error) return reject(error);
+        if (!result) return reject(new Error('Cloudinary upload failed'));
+        resolve(result);
+      });
+      streamifier.createReadStream(file.buffer).pipe(upload);
+    });
+  }
+}
